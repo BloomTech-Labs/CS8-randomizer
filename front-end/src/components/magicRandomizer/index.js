8d23f2d943d8e25365f95fed18a557c004faedce
Unknown <opticalfalicy@gmail.com>
2018-07-16 17:40:56 -0500

    Branch Initiate

---
+++ front-end/src/components/magicRandomizer/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+import './magicRandomizer.css'
+
+class MagicRandomizer extends Component{
+
+    render(){
+        return(
+            <div class='participated'>Participated</div>
+        )
+    }
+}
+
+export default MagicRandomizer;
---
+++ front-end/src/navigation/SideBar/index.js
@@ -0,0 +1,45 @@
+import React from 'react';
+// import { NavLink } from 'react-router-dom';
+
+import { Nav, NavItem, NavLink, Alert, Dropdown, DropdownToggle, DropdownMenu, DropdownItem } from 'reactstrap';
+import './index.css';
+// import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+export default class SideBar extends React.Component {
+    constructor(props) {
+        super(props);
+    
+        this.toggle = this.toggle.bind(this);
+        this.state = {
+          dropdownOpen: false
+        };
+      }
+    
+      toggle() {
+        this.setState({
+          dropdownOpen: !this.state.dropdownOpen
+        });
+      }
+
+render(){
+        return (
+            <div className="SideDiv">
+                <Nav vertical>
+                    <NavItem>
+                        <NavLink href="/Classes" active>Games</NavLink>
+                    </NavItem>
+                    <NavItem>
+                        <NavLink href="/Biling">Biling</NavLink>
+                    </NavItem>
+                    <NavItem>
+                        <NavLink href="/Settings">Settings</NavLink>
+                    </NavItem>
+                </Nav>
+        </div>
+
+        )
+    }
+}
+
+// export default NavBar;
