4bbe004882a1ae80ed5402cdb0022e7bc1ec3893
Unknown <opticalfalicy@gmail.com>
2018-07-17 12:00:02 -0500

    Breadcrumbs for Static Pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,20 +3,49 @@ import axios from "axios";
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 import SideBar from './navigation/SideBar';
-import TopBar from './navigation/TopBar';
+// import TopBar from './navigation/TopBar';
 import { Landing } from "./components";
 import { MagicRandomizer } from "./components";
+import Breadcrumbs from 'react-router-dynamic-breadcrumbs';   
 
 import './App.css';
 
+const routes = {
+  '/': 'Home',
+  '/classes': 'Classes',
+  '/classes/:c_id': ':c_id',
+  '/classes/magicrandomizer': 'Game',
+  '/classes/edit': 'Edit Class',
+  '/classes/create': 'Create Class',
+  '/billing': 'Billing',
+  '/settings': 'Settings'
+}
+
 class App extends Component {
+
+  constructor(){
+    super();
+    this.state={
+      isAuth: true
+      }
+    }
+
+
   render() {
+
+    // Hides navbar when not logged in
+    let loggedSide = this.state.isAuth ? <SideBar /> : '';
+    
+
     return (
       <Router>
       <div className="App">
-        <TopBar className="TopBar" />
-        <SideBar className="SideBar" />
+        <div className='CrumbBar'>
+          <Breadcrumbs id="Crumb" mappedRoutes={routes} />
+        </div>
+        {loggedSide}
         <Route exact path={'/'} render={() => <Landing />} />
+        <Route exact path={'/home'} />
         <Route exact path={'/magic-randomizer'} render={() => <MagicRandomizer />} />
         <Route exact path={'/create'} />
         <Route exact path={'/edit'} />
@@ -28,5 +57,6 @@ class App extends Component {
     );
   }
 }
+
 
 export default App;
