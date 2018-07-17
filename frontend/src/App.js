b16d9d7acb59a5b25a112e89618b2251e56ca910
Unknown <opticalfalicy@gmail.com>
2018-07-17 12:58:44 -0500

    Logout works, plus login test

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import axios from "axios";
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 import SideBar from './navigation/SideBar';
+import LogOut from './navigation/LogOut';
 // import TopBar from './navigation/TopBar';
 import { Landing } from "./components";
 import { MagicRandomizer } from "./components";
@@ -25,33 +26,47 @@ class App extends Component {
 
   constructor(){
     super();
+    this.test = this.test.bind(this);
     this.state={
-      isAuth: true
+      isAuth: false
       }
     }
-
+  
+  test() {
+    this.setState({
+      isAuth: !this.state.isAuth
+    })
+  }
 
   render() {
 
     // Hides navbar when not logged in
     let loggedSide = this.state.isAuth ? <SideBar /> : '';
-    
-
+    let loggedOut = this.state.isAuth ? <LogOut className='Logout'/> : '';
+    let logTest = this.state.isAuth ? '' : <button onClick={this.test} />;
+ 
     return (
       <Router>
       <div className="App">
+        <div className='LogBar'>
+          {/* <LogOut className='Logout' /> */}
+          {loggedOut}
+        </div>
         <div className='CrumbBar'>
           <Breadcrumbs id="Crumb" mappedRoutes={routes} />
         </div>
         {loggedSide}
-        <Route exact path={'/'} render={() => <Landing />} />
-        <Route exact path={'/home'} />
-        <Route exact path={'/magic-randomizer'} render={() => <MagicRandomizer />} />
-        <Route exact path={'/create'} />
-        <Route exact path={'/edit'} />
-        <Route exact path={'/classlist'} />
-        <Route exact path={'/billing'} />
-        <Route exact path={'/settings'} />
+        {logTest}
+        <div>
+          <Route exact path={'/'} render={() => <Landing />} />
+          <Route exact path={'/home'} />
+          <Route exact path={'/magic-randomizer'} render={() => <MagicRandomizer />} />
+          <Route exact path={'/create'} />
+          <Route exact path={'/edit'} />
+          <Route exact path={'/classlist'} />
+          <Route exact path={'/billing'} />
+          <Route exact path={'/settings'} />
+        </div>
       </div>
       </Router>
     );
