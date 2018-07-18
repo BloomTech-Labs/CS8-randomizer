998006643a357d407121306f62d1c850f13619de
WalterSWoodward <walter.sekope.woodward@gmail.com>
2018-07-18 18:22:37 -0400

    User and Class Models Built Authentication is functioning, but there is duplicate code which needs to be cleaned up Student Model needs to be built bcrypt on passwords functioning as expected

--- server.js
+++ server.js
@@ -19,17 +19,19 @@ const corsOptions = {
 };
 
 // ======== ROUTER requires ======== //
+
 // Note
-// const createNoteRouter = require('./Note/createNoteRouter.js');
-// const findNotesRouter = require('./Note/findNotesRouter.js');
-// const deleteNoteRouter = require('./Note/deleteNoteRouter.js');
-// const updateNoteRouter = require('./Note/updateNoteRouter.js');
+const createClassRouter = require('./Class/createClassRouter.js');
+const findClassesRouter = require('./Class/findClassesRouter.js');
+const deleteClassRouter = require('./Class/deleteClassRouter.js');
+const updateClassRouter = require('./Class/updateClassRouter.js');
+
 // User
-// const registerUserRouter = require('./User/registerUserRouter.js');
-// const loginUserRouter = require('./User/loginUserRouter.js');
-// const findUsersRouter = require('./User/findUsersRouter.js');
-// const deleteUserRouter = require('./User/deleteUserRouter.js');
-// const updateUserRouter = require('./User/updateUserRouter.js');
+const registerUserRouter = require('./User/registerUserRouter.js');
+const loginUserRouter = require('./User/loginUserRouter.js');
+const findUsersRouter = require('./User/findUsersRouter.js');
+const deleteUserRouter = require('./User/deleteUserRouter.js');
+const updateUserRouter = require('./User/updateUserRouter.js');
 
 const server = express();
 
@@ -39,7 +41,7 @@ server.use(helmet());
 server.use(morgan('dev'));
 server.use(cors(corsOptions));
 
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true })
+mongoose.connect('mongodb://lambda_labs_backend:_sJ7X3;{pWfzfMR@ds239681.mlab.com:39681/lambda_labs_backend_database', { useNewUrlParser: true })
 
 .then(() => console.log('\n===connected to mongo===\n'))
 .catch(err =>console.log('not connected'))
@@ -53,16 +55,18 @@ server.get('/', function(req, res) {
 // const setupRoutes = require('./setup/routes')(server);
 
 // ======== ROUTERS .use ========== //
-// // Note
-// server.use('/api/createnote', createNoteRouter);
-// server.use('/api/viewnotes', findNotesRouter);
-// server.use('/api/deletenote', deleteNoteRouter);
-// server.use('/api/updatenote', updateNoteRouter);
-// // User
-// server.use('/api/register', registerUserRouter);
-// server.use('/api/login', loginUserRouter);
-// server.use('/api/viewusers', findUsersRouter);
-// server.use('/api/deleteuser', deleteUserRouter);
-// server.use('/api/updateuser', updateUserRouter);
+
+// Class
+server.use('/api/createclass', createClassRouter);
+server.use('/api/viewclasses', findClassesRouter);
+server.use('/api/deleteclass', deleteClassRouter);
+server.use('/api/updateclass', updateClassRouter);
+
+// User
+server.use('/api/register', registerUserRouter);
+server.use('/api/login', loginUserRouter);
+server.use('/api/viewusers', findUsersRouter);
+server.use('/api/deleteuser', deleteUserRouter);
+server.use('/api/updateuser', updateUserRouter);
 
 server.listen(port, () => console.log('API on port 5000...or wherever Heroku is'));
