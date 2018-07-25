a7d0298522105095433e90a2a314a46887161106
WalterSWoodward <walter.sekope.woodward@gmail.com>
2018-07-25 12:38:01 -0400

    adf

--- Schemas/Class.js
+++ Schemas/Class.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const ObjectId = mongoose.Schema.Types.ObjectId;
 
 const classSchema = new mongoose.Schema({
-  name: {
+  class_name: {
     type: String,
     require: true,
     unique: true,
