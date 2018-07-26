956bfa91b9bfde63df5048ff55fcb2a6d91156f5
WalterSWoodward <walter.sekope.woodward@gmail.com>
2018-07-26 17:13:44 -0400

    adsf

--- Router/Class/findClassesRouter.js
+++ Router/Class/findClassesRouter.js
@@ -2,12 +2,10 @@ const express = require('express');
 
 //schema
 const Class = require('../../Schemas/Class.js');
-// authentication
-const { authenticate } = require('../../authenticate');
 
 const router = express.Router();
 
-router.route('/').get(authenticate, (req, res) => {
+router.route('/').get((req, res) => {
   Class.find({})
     .populate('student')
     .then(classes => {
