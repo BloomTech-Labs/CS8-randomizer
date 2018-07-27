15ac508e1aff002dd85399196f2ef8799add5844
WalterSWoodward <walter.sekope.woodward@gmail.com>
2018-07-27 11:44:49 -0400

    add auth to register

--- Router/User/registerUserRouter.js
+++ Router/User/registerUserRouter.js
@@ -25,63 +25,63 @@ function makeToken(user) {
   return jwt.sign(payload, secret, options);
 }
 
-// const localStrategy = new LocalStrategy(function(username, password, done) {
-//   User.findOne({ username }, function(err, user) {
-//     if (err) {
-//       done(err);
-//     }
-//     if (!user) {
-//       done(null, false);
-//     }
+const localStrategy = new LocalStrategy(function(username, password, done) {
+  User.findOne({ username }, function(err, user) {
+    if (err) {
+      done(err);
+    }
+    if (!user) {
+      done(null, false);
+    }
 
-//     user.verifyPassword(password, function(err, isValid) {
-//       if (err) {
-//         return done(err);
-//       }
-//       if (isValid) {
-//         const { _id, username } = user;
-//         return done(null, { _id, username });
-//       }
-//       return done(null, false);
-//     });
-//   });
-// });
+    user.verifyPassword(password, function(err, isValid) {
+      if (err) {
+        return done(err);
+      }
+      if (isValid) {
+        const { _id, username } = user;
+        return done(null, { _id, username });
+      }
+      return done(null, false);
+    });
+  });
+});
 
-// const jwtOptions = {
-//   jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+const jwtOptions = {
+  jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
 
-//   secretOrKey: secret
-// };
+  secretOrKey: secret
+};
 
-// const jwtStrategy = new JwtStrategy(jwtOptions, function(payload, done) {
-//   User.findById(payload.sub)
+const jwtStrategy = new JwtStrategy(jwtOptions, function(payload, done) {
+  User.findById(payload.sub)
 
-//     .select('-password')
-//     .then(user => {
-//       if (user) {
-//         done(null, user);
-//       } else {
-//         done(null, false);
-//       }
-//     })
-//     .catch(err => {
-//       return done(err, false);
-//     });
-// });
+    .select('-password')
+    .then(user => {
+      if (user) {
+        done(null, user);
+      } else {
+        done(null, false);
+      }
+    })
+    .catch(err => {
+      return done(err, false);
+    });
+});
 
-// passport.use(localStrategy);
-// passport.use(jwtStrategy);
+passport.use(localStrategy);
+passport.use(jwtStrategy);
 
-// const authenticate = passport.authenticate('local', { session: false });
-// // This is what you will use to protect user's notes -- see Auth Code Along hobbits [POST] for example
-// const protected = passport.authenticate('jwt', { session: false });
+const authenticate = passport.authenticate('local', { session: false });
+// This is what you will use to protect user's notes -- see Auth Code Along hobbits [POST] for example
+const protected = passport.authenticate('jwt', { session: false });
 // ============ from ROUTES -- END ============= //
 
 //schema
 const User = require('../../Schemas/User.js');
 
 //endpoints
-router.post('/', function(req, res) {
+router.post('/', authenticate, function(req, res) {
   const credentials = req.body;
   const user = new User(credentials);
   
