233533e99b0fe8b485e200ae006c8112e15ea746
swedishgoodbye <swedishgoodbye@gmail.com>
2018-08-02 12:21:58 -0500

    Saves over commit

--- backend/Router/Student/createStudentRouter.js
+++ backend/Router/Student/createStudentRouter.js
@@ -6,7 +6,6 @@ const Student = require('../../Schemas/Student.js');
 
 //endpoints
 // This is just for quick checking
-<<<<<<< HEAD:backend/Router/Student/createStudentRouter.js
 router.route('/').get((req, res) => {
   Student.find({})
     .then(students => {
@@ -23,26 +22,6 @@ router.post('/', (req, res) => {
   const student = req.body;
   Student.create(student)
     .then(student => res.status(201).json('Saved new student'))
-=======
-// router.route('/').get((req, res) => {
-//   Class.find({})
-//     .then(classes => {
-//       if (classes.length === 0) {
-//         res.status(404).json({ error: 'No classes found!' });
-//       } else {
-//         res.status(200).json(classes);
-//       }
-//     })
-//     .catch(error => res.status(500).json(`Error from server: ${error}`));
-// });
-
-router.post('/', (req, res) => {
-  const boat = req.body;
-  console.log('REQ.USER', req.user)
-  console.log(boat)
-  Class.create(boat)
-    .then(boat => res.status(201).json('Saved new class'))
->>>>>>> 56ecdf5038036926fa23fd5a2b5b5b4356ecd094:Router/Class/createClassRouter.js
     .catch(error => res.status(500).json(`Error from server: ${error}`));
 });
 
